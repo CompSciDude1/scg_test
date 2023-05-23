ad72bd11764eb289c7a27e7696fe7f079c1a2d38
Thomas Smith <thomas.smith.42@outlook.com>
2023-05-22 19:35:24 -0500

    scg_test.cpp:  read scg_test.pb and print as JSON to console

--- scg_test.cpp
+++ scg_test.cpp
@@ -3,10 +3,13 @@
 // Purpose:  Output scg_test.pb in JSON format
 
 #include <cstdlib>
+#include <fstream>
+
+#include <google/protobuf/util/json_util.h>
 
 #include "protodef/scg_test.pb.h"
 
-int main(int a_iArgc, char** a_ppchArgv) {
+void TestSCG_TestMessage() {
 	// Create a SCG::Test::TestMessage
 	SCG::Test::TestMessage oScgMessage;
 
@@ -27,6 +30,28 @@ int main(int a_iArgc, char** a_ppchArgv) {
 	// Print the members
 	std::cout << "Timestamp: " << oTimestamp.DebugString()
 		<< "\nNotes: " << oNotes << std::endl;
+}
+
+int main(int a_iArgc, char** a_ppchArgv) {
+	//TestSCG_TestMessage();
+
+	std::string szPROTO_MESSAGE{ "../source/protomessage/scg_test.pb" };
+	std::ifstream ifs;
+	ifs.open(szPROTO_MESSAGE);
+	if (!ifs.is_open()) {
+		std::cout << "Failed to open " << szPROTO_MESSAGE << '\n';
+		return EXIT_FAILURE;
+	}
+	SCG::Test::TestMessage oScgMessage;
+	oScgMessage.ParseFromIstream(&ifs);
+	std::string szScgMessageJson;
+	google::protobuf::util::MessageToJsonString(oScgMessage, &szScgMessageJson);
+	std::cout << "SCG::Test::TestMessage oScgMessage as JSON string\n" << szScgMessageJson << '\n';
+	ifs.close();
+	if (ifs.is_open()) {
+		std::cout << "Failed to close " << szPROTO_MESSAGE << '\n';
+		return EXIT_FAILURE;
+	}
 
 	return EXIT_SUCCESS;
 }
