acb1037ac7381962d4ba32ca6897f371305d5fdc
Thomas Smith <thomas.smith.42@outlook.com>
2023-05-23 18:11:52 -0500

    Program.cs:  add TestSCG_TestMessage

--- source_cs/Program.cs
+++ source_cs/Program.cs
@@ -1,11 +1,30 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using Google.Protobuf;
+using System.Text;
 
 public class Program
 {
+    private static void TestSCG_TestMessage()
+    {
+        // Create a SCG.Test.TestMessage
+        var oScgMessage = new SCG.Test.TestMessage();
+
+        // Set the members
+        {
+            var oTimeNow = Google.Protobuf.WellKnownTypes.Timestamp.FromDateTime(DateTime.UtcNow);
+            oScgMessage.Timestamp = oTimeNow;
+            string oNotes = "Hello World!";
+            oScgMessage.Notes = oNotes;
+        }
+
+        // Print the members
+        System.Console.WriteLine("Timestamp: " + oScgMessage.Timestamp);
+        System.Console.WriteLine("Notes: " + oScgMessage.Notes);
+    }
     public static void Main(string[] args)
     {
+        //TestSCG_TestMessage();
         string szPROTO_MESSAGE = "protomessage/scg_test.pb";
         System.IO.FileStream oStream = System.IO.File.Open(szPROTO_MESSAGE, FileMode.Open);
         if (oStream.CanRead == false )
