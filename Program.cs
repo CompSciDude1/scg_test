aa1e98c1ad89fba2c14720d283d5b73b39f0f6fc
Thomas Smith <thomas.smith.42@outlook.com>
2023-05-23 02:14:29 -0500

    Program.cs:  read scg_test.pb and print as JSON to console

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using Google.Protobuf;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        string szPROTO_MESSAGE = "protomessage/scg_test.pb";
+        System.IO.FileStream oStream = System.IO.File.Open(szPROTO_MESSAGE, FileMode.Open);
+        if (oStream.CanRead == false )
+        {
+            System.Console.WriteLine("Failed to open " +  szPROTO_MESSAGE);
+            return;
+        }
+        SCG.Test.TestMessage oScgMessage = SCG.Test.TestMessage.Parser.ParseFrom(oStream);
+        System.Console.WriteLine("SCG.Test.TestMessage oScgMessage as JSON string");
+        var oJsonWriter = new JsonFormatter(JsonFormatter.Settings.Default);
+        System.Console.WriteLine(oJsonWriter.Format(oScgMessage));
+    }
+}
