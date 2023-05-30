67220071f3d4966d2a408c8ee326df5c3e08ca21
Thomas Smith <thomas.smith.42@outlook.com>
2023-05-30 02:33:47 -0500

    scg_test.cpp:  make JSON output pretty
      Pass to MessageToJsonString a JsonOptions with add_whitespace set to true to make the JSON output pretty.
      Changed a std::endl to '\n'.
      Renamed ifs to oIfs to follow variable naming convention in the file.
      Commented the code.

--- source_cpp/scg_test.cpp
+++ source_cpp/scg_test.cpp
@@ -29,26 +29,38 @@ void TestSCG_TestMessage() {
 
 	// Print the members
 	std::cout << "Timestamp: " << oTimestamp.DebugString()
-		<< "\nNotes: " << oNotes << std::endl;
+		<< "\nNotes: " << oNotes << '\n';
 }
 
 int main(int a_iArgc, char** a_ppchArgv) {
 	//TestSCG_TestMessage();
-
+	
+	// Open scg_test.pb
 	std::string szPROTO_MESSAGE{ "protomessage/scg_test.pb" };
-	std::ifstream ifs;
-	ifs.open(szPROTO_MESSAGE);
-	if (!ifs.is_open()) {
+	std::ifstream oIfs;
+	oIfs.open(szPROTO_MESSAGE);
+	if (!oIfs.is_open()) {
 		std::cout << "Failed to open " << szPROTO_MESSAGE << '\n';
 		return EXIT_FAILURE;
 	}
+
+	// Read message
 	SCG::Test::TestMessage oScgMessage;
-	oScgMessage.ParseFromIstream(&ifs);
+	oScgMessage.ParseFromIstream(&oIfs);
+
+	// Convert to JSON
 	std::string szScgMessageJson;
-	google::protobuf::util::MessageToJsonString(oScgMessage, &szScgMessageJson);
+	google::protobuf::util::JsonOptions oJsonOptions;
+	// Make the JSON pretty
+	oJsonOptions.add_whitespace = true;
+	google::protobuf::util::MessageToJsonString(oScgMessage, &szScgMessageJson, oJsonOptions);
+
+	// Print message in JSON format
 	std::cout << "SCG::Test::TestMessage oScgMessage as JSON string\n" << szScgMessageJson << '\n';
-	ifs.close();
-	if (ifs.is_open()) {
+
+	// Close scg_test.pb
+	oIfs.close();
+	if (oIfs.is_open()) {
 		std::cout << "Failed to close " << szPROTO_MESSAGE << '\n';
 		return EXIT_FAILURE;
 	}
